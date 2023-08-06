7bd4970c45cf5604953305282b8749ce4a3025ff
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-08-06 21:09:16 +0200

    add styling to contentlist

--- components/contentlist/index.js
+++ components/contentlist/index.js
@@ -2,6 +2,12 @@ import Image from "next/image";
 import data from "../../lib/data.js";
 import styled from "styled-components";
 
+
+const StyledContentlist = styled.ul`
+list-style-type: none;
+border: 2px solid lightgrey; 
+`;
+
 const StyledH1 = styled.h1`
   color: #BF4F74;
 `;
@@ -15,14 +21,14 @@ export default function Contentlist() {
   return (
     <StyledDIV>
       <StyledH1>Contentliste</StyledH1>
-      <ul>
+      <StyledContentlist>
         {data.map((item) => (
           <li key={item.id}>
             <h2>{item.title}</h2>
             <p>{item.previewtext}</p>
           </li>
         ))}
-      </ul>
+      </StyledContentlist>
     </StyledDIV>
   );
 }
