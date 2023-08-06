8f69407208b1bfc1c5d076b699019b235dc7bc3a
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-08-06 20:31:43 +0200

    add component

--- components/contentlist/index.js
+++ components/contentlist/index.js
@@ -1,5 +1,23 @@
-import next from "next/image"
+import Image from "next/image";
+import data from "../../lib/data.js";
+import styled from "styled-components";
 
-export default function Contentlist ({content}) {
+const StyledH1 = styled.h1`
+  text-align: center;
+`;
 
+export default function Contentlist() {
+  return (
+    <div>
+      <StyledH1>Contentliste</StyledH1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.previewtext}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import ContentList from "../components/contentlist/index.js";
+
 export default function Home() {
-    return <h1>Willkommen bei keineahnungbre</h1>;
-  }
+  return (
+    <>
+      <h1>Willkommen bei keineahnungbre</h1>
+      <div>
+        <ContentList />
+      </div>
+    </>
+  );
+}
